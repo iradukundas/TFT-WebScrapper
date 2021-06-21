50ebd6d41f699b6247f72b50bd1a81035ffb6b6e
Samuel Iradukunda <gadisamuel1512@gmail.com>
2021-06-21 19:05:05 -0400

    3rd

--- main.py
+++ main.py
@@ -8,13 +8,7 @@ import discord
 import os
 
 #Setting up for webscrapping
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-
+driver = webdriver.Chrome(executable_path = "/app/.chromedriver/bin/chromedriver")
 
 #discord side
 Client = commands.Bot(command_prefix = '!')
