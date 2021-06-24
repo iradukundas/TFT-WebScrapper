3c60037804023f5eb572cf92cf554dc84306ec58
Samuel Iradukunda <gadisamuel1512@gmail.com>
2021-06-24 08:10:08 -0400

    init

--- main.py
+++ main.py
@@ -128,4 +128,7 @@ async def lb(ctx):
     driver.get("https://lolchess.gg")
 
 
-Client.run('Nzc4MzEzMDg3NzIyOTc5MzU4.X7QKjA.jaekVheDNe3rTRwFuqbH3aOVCMg')
+Client.run('')
+
+# git add ., git commit -m "msg", git push heroku <--- for changes
+#heroku logs -a discord-tft-stats --tail
